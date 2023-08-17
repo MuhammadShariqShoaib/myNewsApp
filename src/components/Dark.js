0a4f66339ff0035b093328ff230244ff1732b7a5
MuhammadShariqShoaib <134840447+MuhammadShariqShoaib@users.noreply.github.com>
2023-08-17 12:03:00 +0500

    Update Dark.js(notused)

--- src/components/Dark.js
+++ src/components/Dark.js
@@ -1,58 +1,58 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class DarkModeButton extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            button: 'Enable Dark Mode',
-            mystyle: {
-                color: 'black',
-                backgroundColor: 'white',
-            },
-        };
-    }
+// class DarkModeButton extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             button: 'Enable Dark Mode',
+//             mystyle: {
+//                 color: 'black',
+//                 backgroundColor: 'white',
+//             },
+//         };
+//     }
 
-    toggleState = () => {
-        const { mystyle } = this.state;
-        if (mystyle.backgroundColor === 'white') {
-            this.setState({
-                mystyle: {
-                    color: 'white',
-                    backgroundColor: 'black',
-                    border: '1px solid white',
-                },
-                button: 'Disable Dark Mode',
-            });
-        } else {
-            this.setState({
-                mystyle: {
-                    color: 'black',
-                    backgroundColor: 'white',
-                },
-                button: 'Enable Dark Mode',
-            });
-        }
-    };
+//     toggleState = () => {
+//         const { mystyle } = this.state;
+//         if (mystyle.backgroundColor === 'white') {
+//             this.setState({
+//                 mystyle: {
+//                     color: 'white',
+//                     backgroundColor: 'black',
+//                     border: '1px solid white',
+//                 },
+//                 button: 'Disable Dark Mode',
+//             });
+//         } else {
+//             this.setState({
+//                 mystyle: {
+//                     color: 'black',
+//                     backgroundColor: 'white',
+//                 },
+//                 button: 'Enable Dark Mode',
+//             });
+//         }
+//     };
 
-    render() {
-        const { button, mystyle } = this.state;
+//     render() {
+//         const { button, mystyle } = this.state;
 
-        return (
-            <div>
-                <button onClick={this.toggleState}>{button}</button>
-                <div
-                    style={{
-                        color: mystyle.color,
-                        backgroundColor: mystyle.backgroundColor,
-                        border: mystyle.border,
-                        padding: '10px',
-                        margin: '10px',
-                    }}
-                >
+//         return (
+//             <div>
+//                 <button onClick={this.toggleState}>{button}</button>
+//                 <div
+//                     style={{
+//                         color: mystyle.color,
+//                         backgroundColor: mystyle.backgroundColor,
+//                         border: mystyle.border,
+//                         padding: '10px',
+//                         margin: '10px',
+//                     }}
+//                 >
                     
-                </div>
-            </div>
-        );
+//                 </div>
+//             </div>
+//         );
     }
 }
 
